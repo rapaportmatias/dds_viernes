2e632b666c505090d98158e83dc9fa62be67563a
rapaportmatias <rapaport.matias@gmail.com>
2017-04-13 19:53:15 -0200

    no hice ninguna modificacion pertinente, quiero acer pull pero me pide que aga push primero....

--- src/main/java/dds_viernes/LectorNotasApplication.java
+++ src/main/java/dds_viernes/LectorNotasApplication.java
@@ -4,11 +4,16 @@ import org.uqbar.arena.Application;
 import org.uqbar.arena.windows.Window;
 
 import dds_viernes.ui.windows.LoginWindow;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
 
 public class LectorNotasApplication extends Application{
 
 	public static void main(String[] args) {		
 		new LectorNotasApplication().start();
+		 
 	}
 	
 	@Override
