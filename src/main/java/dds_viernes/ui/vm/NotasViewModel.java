a05e72b0fcd8ad727e48e7d9c02e0871b56942b7
BiancoN <bianco.nicolasm@gmail.com>
2017-04-14 17:52:30 -0300

    NotasVM

--- src/main/java/dds_viernes/ui/vm/NotasViewModel.java
+++ src/main/java/dds_viernes/ui/vm/NotasViewModel.java
@@ -1,5 +1,24 @@
 package dds_viernes.ui.vm;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import dominio.Asignacion;
+
 public class NotasViewModel {
 
+	private String token;
+	private List<Asignacion> asignaciones;
+
+	public NotasViewModel() {
+		asignaciones = new ArrayList<>();
+	}
+	
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
 }
