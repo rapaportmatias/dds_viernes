f924835c69075892a52e2d43cb922c9e5bccf2be
Maxi <maxi.fernandez.95@hotmail.com>
2017-04-14 00:34:40 -0300

    Ventana menu

--- src/main/java/dds_viernes/ui/windows/MenuWindow.java
+++ src/main/java/dds_viernes/ui/windows/MenuWindow.java
@@ -1,7 +1,12 @@
 package dds_viernes.ui.windows;
 
+import java.awt.Color;
+
+
+import org.uqbar.arena.layout.HorizontalLayout;
 import org.uqbar.arena.layout.VerticalLayout;
 import org.uqbar.arena.widgets.Button;
+import org.uqbar.arena.widgets.Label;
 import org.uqbar.arena.widgets.Panel;
 import org.uqbar.arena.windows.Dialog;
 import org.uqbar.arena.windows.SimpleWindow;
@@ -9,13 +14,13 @@ import org.uqbar.arena.windows.WindowOwner;
 
 import dds_viernes.ui.vm.MenuViewModel;
 
-public class MenuWindow extends SimpleWindow<MenuViewModel> {
+public class MenuWindow extends SimpleWindow<MenuViewModel>{
 		
 	private static final long serialVersionUID = 1L;
 
 	private Button notasButton;
 	private Button datosButton;
-	
+
 	public MenuWindow(WindowOwner parent) {
 		super(parent, new MenuViewModel());
 	}
@@ -23,9 +28,14 @@ public class MenuWindow extends SimpleWindow<MenuViewModel> {
 	@Override
 	protected void createFormPanel(Panel mainPanel) {
 		this.setTitle("Menu");
-		mainPanel.setLayout(new VerticalLayout());	
-		notasButton = new Button(mainPanel).setCaption("Ver notas");
-		datosButton = new Button(mainPanel).setCaption("Actualizar datos");
+		mainPanel.setLayout(new HorizontalLayout());
+		Panel izqPanel = new Panel(mainPanel);
+		Panel derPanel = new Panel(mainPanel);
+		notasButton = new Button(izqPanel).setCaption("Ver Notas");
+		datosButton = new Button(izqPanel).setCaption("Actualizar Datos");
+		new Label(derPanel).setBackground(Color.PINK).setWidth(300).setHeight(200);
+		
+		
 	}
 	
 	public void abrirNotas() {
@@ -45,4 +55,5 @@ public class MenuWindow extends SimpleWindow<MenuViewModel> {
 		notasButton.onClick(this::abrirNotas);
 		datosButton.onClick(this::abrirDatos);
 	}
+	
 }
